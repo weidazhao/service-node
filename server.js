c618bfeb01fac5c21e802b30016ee7a5bad32ca2
Saurabh Sarkar <saurabsa@microsoft.com>
2016-07-14 11:50:53 -0700

    Revert back to Initial files
    For CTI

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');     // load the single view file (angular will handle the page changes on the front-end)
 });
 
-var port = process.env.PORT || 4000;
+var port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log("Listening on port " + port);
 });
